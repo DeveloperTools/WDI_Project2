61c46594ad9f7e284b1b12ffb74ab94d3ec25a4b
Jason Tham <jtham@sbcglobal.net>
2015-12-08 10:03:24 -0600

    made some changes to draft controller

--- controllers/draft.rb
+++ controllers/draft.rb
@@ -1,7 +1,16 @@
 class DraftController < ApplicationController
 
 get '/' do
+  @user = Account.find_by(account_name: session[:current_user])
   erb :draft
 end
+
+get '/live' do
+  @draft = Draft.create(
+    :team_owner => params[:user_name]
+
+  )
+end
+
 
 end
