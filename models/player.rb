96f5c8212d0a52d28297d638ecb569385f69895a
Jason Tham <jtham@sbcglobal.net>
2015-12-08 12:50:03 -0600

    Player queries working for draft

--- models/player.rb
+++ models/player.rb
@@ -1,3 +1,11 @@
 class Player < ActiveRecord::Base
   self.table_name = 'master'
 end
+
+class Batter < ActiveRecord::Base
+  self.table_name = 'batting'
+end
+
+class Pitcher < ActiveRecord::Base
+  self.table_name = 'pitching'
+end
