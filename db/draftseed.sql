14085125597e0d9250e6bf52cf20bb2a93bcb5de
Jason Tham <jtham@sbcglobal.net>
2015-12-09 10:00:33 -0600

    added league mvc stuff

--- db/draftseed.sql
+++ db/draftseed.sql
@@ -1,1 +1,1 @@
-create table drafts (id SERIAL PRIMARY KEY, team_owner varchar(255), team_name varchar(255), league_name varchar(255), player1_id varchar(255), player1_yearid varchar(255));
+create table drafts (id SERIAL PRIMARY KEY, team_owner varchar(255), team_name varchar(255), league_name varchar(255), player1_id varchar(255), player1_yearid varchar(255), R_total integer, H_total integer, AB_total integer, HR_total integer, RBI_total integer, SB_total integer);
