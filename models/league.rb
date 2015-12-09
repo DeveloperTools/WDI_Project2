ad0285f3631619a90b9d430112db3e7fabe334ea
Jason Tham <jtham@sbcglobal.net>
2015-12-09 00:22:01 -0600

    added league draft associations

--- models/league.rb
+++ models/league.rb
@@ -1,5 +1,4 @@
 class League < ActiveRecord::Base
   self.table_name = 'leagues'
-  has_many :accounts, class_name: :Draft, foreign_key: :team_owner
-  has_many :teams, class_name: :Draft, foreign_key: :team_name
+  has_many :teams, class_name: :Draft, foreign_key: :league_name
 end
