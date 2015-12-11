3a0697764dcec0382cb9a26ae81609f1da3ba28b
Martin Ryan <martinrryan@gmail.com>
2015-12-11 09:42:25 -0600

    sep js for league_detail

---
+++ public/script/league_detail_ajax.js
@@ -0,0 +1,86 @@
+$.ajax({
+  url: url,
+  type: 'GET',
+  dataType: 'json'
+})
+.done(function(data) {
+  console.log("success");
+  teamjson = data;
+  console.log(data);
+  var $team1 = $("<div>", {class: "teamName"}).text("Team 1 owned by:");
+  $('#results').append($team1);
+
+  $('#results').append('Batters');
+  for (var i = 1; i < 10 ; i++) {
+    var pid = "player" + i + "_id";
+    var $player = $("<div>", {class: "playerContainer", id: "player" + i});
+    var $name = $("<div>", {class: "playerName"});
+    $name.text("Batter " + i + ": " + teamjson.team1[i-1]);
+    $player.append($name);
+    $('#results').append($player);
+  }
+  $('#results').append('PITCHERS');
+  for (var i = 10; i < 17 ; i++) {
+    var pid = "player" + i + "_id";
+    var $player = $("<div>", {class: "playerContainer", id: "player" + i});
+    var $name = $("<div>", {class: "playerName"});
+    $name.text("Pitcher " + (i-9) + ": " + teamjson.team1[i-1]);
+    $player.append($name);
+    $('#results').append($player);
+  }
+
+  var $team2 = $("<div>", {class: "teamName"}).text("Team 2 owned by:");
+  $('#results').append($team2);
+
+  $('#results').append('Batters');
+  for (var i = 1; i < 10 ; i++) {
+    var pid = "player" + i + "_id";
+    var $player = $("<div>", {class: "playerContainer", id: "player" + i});
+    var $name = $("<div>", {class: "playerName"});
+    $name.text("Batter " + i + ": " + teamjson.team2[i-1]);
+    $player.append($name);
+    $('#results').append($player);
+  }
+  $('#results').append('PITCHERS');
+  for (var i = 10; i < 17 ; i++) {
+    var pid = "player" + i + "_id";
+    var $player = $("<div>", {class: "playerContainer", id: "player" + i});
+    var $name = $("<div>", {class: "playerName"});
+    $name.text("Pitcher " + (i-9) + ": " + teamjson.team2[i-1]);
+    $player.append($name);
+    $('#results').append($player);
+  }
+  if (teamjson.team1[15] && teamjson.team2[15]){
+  var $simbutton = $("<button>", {class: "simButton", id: "simButton", onclick: "runSimulation();" });
+  $('#results').append($simbutton);
+  console.log("added simbutton");
+  }
+
+})
+.fail(function() {
+  console.log("error");
+})
+.always(function() {
+  console.log("complete");
+});
+
+console.log(teamjson);
+
+function runSimulation() {
+  // body...
+  $.ajax({
+    url: simUrl,
+    type: 'GET'
+  })
+  .done(function(data) {
+    console.log("success");
+    console.log(data);
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+
+}
