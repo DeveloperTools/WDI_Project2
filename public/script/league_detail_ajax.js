0e3ba12473e2f4be0f90389b4214cd82f34661b4
Martin Ryan <martinrryan@gmail.com>
2015-12-11 10:48:31 -0600

    skinning league detail

--- public/script/league_detail_ajax.js
+++ public/script/league_detail_ajax.js
@@ -7,52 +7,54 @@ $.ajax({
   console.log("success");
   teamjson = data;
   console.log(data);
-  var $team1 = $("<div>", {class: "teamName teamOne"});
-  $('#results_team1').append($team1);
 
-  $('#results_team1').append('Batters');
+  var $battersHeader = $("<div>", {class: "battersHeader"}).text("BATTERS");
+  var $pitchersHeader = $("<div>", {class: "pitchersHeader"}).text("PITCHERS");
+
+  var $team1 = $("<div>", {class: "teamName teamOne"}).text(current_account);
+  $('#results_teamOne').append($team1);
+  $('#results_teamOne').append($battersHeader);
   for (var i = 1; i < 10 ; i++) {
     var pid = "player" + i + "_id";
     var $player = $("<div>", {class: "playerContainer", id: "player" + i});
     var $name = $("<div>", {class: "playerName"});
     $name.text("Batter " + i + ": " + teamjson.team1[i-1]);
     $player.append($name);
-    $('#results_team1').append($player);
+    $('#results_teamOne').append($player);
   }
-  $('#results_team1').append('PITCHERS');
+  $('#results_teamOne').append($pitchersHeader);
   for (var i = 10; i < 17 ; i++) {
     var pid = "player" + i + "_id";
     var $player = $("<div>", {class: "playerContainer", id: "player" + i});
     var $name = $("<div>", {class: "playerName"});
     $name.text("Pitcher " + (i-9) + ": " + teamjson.team1[i-1]);
     $player.append($name);
-    $('#results_team1').append($player);
+    $('#results_teamOne').append($player);
   }
-
-  var $team2 = $("<div>", {class: "teamName teamTwo"});
-  $('#results_team2').append($team2);
-
-  $('#results_team2').append('Batters');
+  //
+  var $team2 = $("<div>", {class: "teamName teamTwo"}).text(opponent_account);
+  $('#results_teamTwo').append($team2);
+  $('#results_teamTwo').append($battersHeader);
   for (var i = 1; i < 10 ; i++) {
     var pid = "player" + i + "_id";
     var $player = $("<div>", {class: "playerContainer", id: "player" + i});
     var $name = $("<div>", {class: "playerName"});
     $name.text("Batter " + i + ": " + teamjson.team2[i-1]);
     $player.append($name);
-    $('#results_team2').append($player);
+    $('#results_teamTwo').append($player);
   }
-  $('#results_team2').append('PITCHERS');
+  $('#results_teamTwo').append($pitchersHeader);
   for (var i = 10; i < 17 ; i++) {
     var pid = "player" + i + "_id";
     var $player = $("<div>", {class: "playerContainer", id: "player" + i});
     var $name = $("<div>", {class: "playerName"});
     $name.text("Pitcher " + (i-9) + ": " + teamjson.team2[i-1]);
     $player.append($name);
-    $('#results_team2').append($player);
+    $('#results_teamTwo').append($player);
   }
   if (teamjson.team1[15] && teamjson.team2[15]){
   var $simbutton = $("<button>", {class: "simButton", id: "simButton", onclick: "runSimulation();" });
-  $('#results_team2').append($simbutton);
+  $('#results_teamTwo').append($simbutton);
   console.log("added simbutton");
   }
 
